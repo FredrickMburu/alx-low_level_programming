1f70b836aa1c9a7b061476280934a0d02eae15e3
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2023-06-15 12:16:45 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int j;
 long int m;
 long long int l;
 
-printf("Size of a char: %c byte(s)\n", sizeof(i));
+printf("Size of a char: %d byte(s)\n", sizeof(i));
 printf("Size of a int: %d byte(s)\n", sizeof(j));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
 printf("Size of a long int: %d byte(s)\n", sizeof(m));
