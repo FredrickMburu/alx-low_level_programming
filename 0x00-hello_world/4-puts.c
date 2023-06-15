35c8cfbe725bf73aeed11bd659bffb3c05d82940
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2023-06-15 11:19:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
-
+/**
+ * main: prints string.
+ * return: 0 exits the program.
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
