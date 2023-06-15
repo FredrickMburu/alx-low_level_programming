dec9e760139b114538ab2b0e66b423eb2caa1b89
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2023-06-15 11:34:50 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * Description: main - prints the string.
+ * Return: 0 exits after exeacution.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
