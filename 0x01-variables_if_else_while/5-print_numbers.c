2d102ff290b9bfc5d5de98575a450bd0fe9d2f35
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2023-06-16 12:38:26 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * Description: main - entry point.
+ * Return: 0 exits the program after execution
+ */
+int main(void)
+{
+int j;
+
+for (j = 0; j < 10; j++)
+{
+printf("%d", j);
+}
+printf("\n");
+return (0);
+}
