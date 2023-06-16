c70fbc59e57cde3a3be5417bc26ffdc211965d32
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2023-06-16 15:26:38 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * Description: main - entry point.
+ * Return: 0 exits the program after execution
+ */
+int main(void)
+{
+int i;
+int j;
+
+for (i = 0; i < 99; i++)
+{
+for (j = 0; j < 99; j++)
+{
+if (i < j && i != j)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+if (i != 98 || j !=99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+
+}
