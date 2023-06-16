04f5876c4f3fd0065bc56d99d7d48d8dfa8046bc
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2023-06-16 15:35:56 -0700

    102=print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int j;
 
 for (i = 0; i < 99; i++)
 {
-for (j = 0; j < 99; j++)
+for (j = 0; j <= 99; j++)
 {
 if (i < j && i != j)
 {
@@ -21,7 +21,7 @@ putchar((i % 10) + '0');
 putchar(' ');
 putchar((j / 10) + '0');
 putchar((j % 10) + '0');
-if (i != 98 || j !=99)
+if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
