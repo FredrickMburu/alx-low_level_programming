00fe706a3eb8843a63a148a22d247c73fccc0754
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2023-06-16 12:12:16 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * Description: main - entry point.
+ * Return: 0 exits the program after execution
+ */
+int main(void)
+{
+int j;
+
+for (j = 'a'; j <= 'z'; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
