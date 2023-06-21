83e047c999a385585c1c56f5dd6fb2ab5d8c7a8a
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2023-06-21 11:12:51 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * Description: print_to_98 - print from n to 98.
+ * @n: passes the value  to the print_to_98 function
+ * Return: returns the values to the main,
+ */
+
+
+void print_to_98(int n)
+{
+int i;
+for (i = n; i <= 98; i++)
+{
+_putchar(n);
+_putchar(44);
+_putchar(32);
+}
+}
