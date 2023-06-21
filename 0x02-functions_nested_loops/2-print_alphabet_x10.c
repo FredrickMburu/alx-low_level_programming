e6a05ee79edcb35ff335ec4c09d7c360e6507b04
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2023-06-20 18:05:20 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * Description: print_alphabet_x10 - print alphabet.
+ * Return: 0 exits the program after execution
+ */
+
+void print_alphabet_x10(void)
+{
+int k;
+char j;
+
+for (k = 0; k <= 10; k++)
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
+}
