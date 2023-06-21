4531e5b26955736ddc21bc36823b5b96d4348799
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2023-06-20 17:52:45 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Description: print_alphabet - print alphabet.
+ * Return: 0 exits the program after execution
+ */
+void print_alphabet(void)
+{
+char j;
+
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
