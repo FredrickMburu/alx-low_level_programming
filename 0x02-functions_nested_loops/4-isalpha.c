78f973d652bca1cddcd9c1a76b9b48138f26c88a
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2023-06-20 18:37:01 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * Description: _isalpha - checks for lower and upper case.
+ * @c: - character that is checked.
+ * Return: (0) for non letters  and  (1) for letters.
+ */
+
+
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) && (c >= 65 && c <= 90))
+{
+return (1);
+}
+
+return (0);
+}
