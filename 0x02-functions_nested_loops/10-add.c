a5980a75cff6b6a4513588fd45cbbdeaed805537
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2023-06-21 10:49:46 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * Description: add - adds the values.
+ * @a: value copied to the function.
+ * @b:value copied to the function
+ * Return: (a+b)the result is returned to the main function
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
