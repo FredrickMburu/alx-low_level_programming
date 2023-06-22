5d2ad25ca97b7c9faa2ff06a961b4c120f443f06
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2023-06-22 09:57:16 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * Description: print_numbers - fprint from 0 to 9.
+ *
+ */
+
+void print_numbers(void)
+{
+
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
