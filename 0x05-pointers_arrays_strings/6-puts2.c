5230e54af5d59c8a0ef2cee219813987e9b68a24
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2023-06-26 13:52:39 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - function to print char.
+ * @str: passes string to function
+ */
+
+
+void puts2(char *str)
+{
+	int length;
+
+	length = 0;
+
+	while (str[length])
+	{
+		if (str[length] % 2 == 0)
+			_putchar(str[length]);
+		length++;
+	}
+	_putchar('\n');
+}
+
