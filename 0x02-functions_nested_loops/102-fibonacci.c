a3ca7c6cda40f0ce0e9cac094a45f75289dd61ac
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2023-06-29 10:45:53 -0700

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,7 +6,6 @@
  * Return: (0) terminates program after execution.
  * @n: copies value to the function.
  */
-void fibonacci(int n); 
 int main(void)
 {
 fibonacci(50);
@@ -18,9 +17,9 @@ void fibonacci(int n)
 int a = 1;
 int b = 2;
 printf("%d, %d", a, b);
-int next;
-for (int i = 0; i <= n; i++)
+for (int i =2; i <= n; i++)
 {
+int next;
 next = a + b;
 printf(", %d", next);
 a = b;
