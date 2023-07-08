141a8cded5a27495718d5f1c67b5f9770c430072
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2023-07-07 17:35:08 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry to the program.
+ * @argc: counts the no of argumets in command line.
+ * @argv[]: a char pointer that passes the commands in the 
+ * commmand line to the main function.
+ * Return: (0) exits the program after completion.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
